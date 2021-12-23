cfd31ca6acc2938a53e7b7b0a03f8b0c86ea0d60
MansiVakharia <mansivakharia58@gmail.com>
2021-12-23 16:18:41 -0500

    Initaial Gameboard

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import TicTacToe from './TicTacToe/TicTacToe';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <TicTacToe/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
